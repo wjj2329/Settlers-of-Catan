8a16c978dae1ecb625e03cd7d5f9e55bde7ddbf6
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-05 13:51:03 -0600

    more crap yay

--- src/shared/game/Player.java
+++ src/shared/game/Player.java
@@ -15,8 +15,28 @@ public class Player
 	 */
 	private int numCities = 0;
 	/**
-	 * Color: Color is received as a String from the JSON.
+	 * Color: Color is received as a String from the JSON file.
 	 * However, we need to use the enum type CatanColor.
 	 */
 	private CatanColor color = null;
+	
+	/**
+	 * discarded: whether or not they discarded a card
+	 */
+	private boolean discarded = false;
+	
+	/**
+	 * numMonuments: how many monuments a player has.
+	 */
+	private int numMonuments = 0;
+	
+	/**
+	 * Name: Player's name.
+	 * Examples: Jess, Rob, Tiny, Kahuna (max 7 char)
+	 * 
+	 * According to the JSON this will be read as just a String.
+	 * However, we may need a class to encapsulate "Name" in the future
+	 * in order to avoid primitive obsession. 
+	 */
+	private String name = "";
 }
