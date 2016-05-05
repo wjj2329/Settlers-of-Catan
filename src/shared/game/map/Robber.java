3d0814dfdbef060fc4b20438b81c6d3680cf1f35
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-05 13:30:56 -0600

    more edits to classes robber map etc.

--- src/shared/game/map/Robber.java
+++ src/shared/game/map/Robber.java
@@ -1,5 +1,7 @@
 package shared.game.map;
 
+import shared.locations.HexLocation;
+
 /**
  * @author Alex
  * Robber: Class to represent the Robber, which moves about
@@ -8,5 +10,44 @@ package shared.game.map;
  */
 public class Robber 
 {
+	/**
+	 * location: Represents the Robber's current location
+	 * on the playing board. (i.e., which hex s/he is on)
+	 */
+	private HexLocation location = null;
+	
+	/**
+	 * Robber constructor
+	 * @param initialLocation: where the robber starts
+	 * (should be on the Desert Hex). 
+	 */
+	public Robber(HexLocation initialLocation)
+	{
+		location = initialLocation;
+	}
+	
+	/**
+	 * canPlaceRobber: Determines whether or not the
+	 * robber can be placed at the given HexLocation.
+	 * @param loc: given hex location
+	 */
+	boolean canPlaceRobber(HexLocation loc)
+	{
+		return false;
+	}
 
+	/**
+	 * Getters and Setters follow:
+	 */
+	public HexLocation getLocation() 
+	{
+		return location;
+	}
+
+	public void setLocation(HexLocation location) 
+	{
+		this.location = location;
+	}
+	
+	
 }
