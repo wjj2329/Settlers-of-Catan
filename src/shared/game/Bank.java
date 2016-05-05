91e5786c4d80e8674d6616e435e715931460e67b
wjj2329 <wjj2329@gmail.com>
2016-05-05 16:59:59 -0600

    moved some stuff arround

--- src/shared/game/Bank.java
+++ src/shared/game/Bank.java
@@ -9,12 +9,16 @@ public class Bank
 	 */
 	ArrayList<Card>mycards=new ArrayList<>();
 	
-	
+	/**
+	 *  A function that checks to see if the Bank can even give a card to a player
+	 */
 	boolean HasCardAvailable(String cardtype)
 	{
 		return true;
 	}
-	
+	/**
+	 *  A function that gives a player a card
+	 */
 	Card giveplayercard()
 	{
 		Card cardtogive=null;
