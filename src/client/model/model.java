e1f04745612971416c5c92886558db0edc3c3a33
wjj2329 <wjj2329@gmail.com>
2016-05-05 13:42:49 -0600

    Model stuff

---
+++ src/client/model/model.java
@@ -0,0 +1,40 @@
+package client.model;
+
+import java.util.ArrayList;
+
+import shared.game.Bank;
+import shared.game.map.Map;
+
+public class model 
+{
+public model()
+{
+	
+}
+public void createGame()
+{
+	
+}
+
+public void listGames()
+{
+	
+}
+public void joinGame()
+{
+	
+}
+public void serializeModel()
+{
+	
+}
+public void updatFromJSON()
+{
+	
+}
+Bank bank;
+Map map;
+//ArrayList<Player>myplayers;
+
+
+}
