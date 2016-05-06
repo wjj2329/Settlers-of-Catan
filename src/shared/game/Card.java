edb59a1e483e11fff1c888163d885c0a99968ed1
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-05 20:38:21 -0600

    wiwi

--- src/shared/game/Card.java
+++ src/shared/game/Card.java
@@ -1,9 +1,17 @@
 package shared.game;
 
+/**
+ * @author William
+ * Class to encapsulate the different cards that the players use.
+ */
 public class Card 
 {
-	String type;
-
+	String type = "";
+	/**
+	 * Constructor for Card.
+	 * @param type: the type of the card. Will probably change to an enum
+	 * from the premade classes.
+	 */
 	public Card(String type)
 	{
 		this.type=type;
