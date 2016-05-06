e18a42fdef4bf0c2d6264a3a966f41094e1856e3
guzmanz <zulguzman@yahoo.com>
2016-05-06 10:12:38 -0600

    accidnetly did something changing it back

--- src/shared/game/CatanGame.java
+++ src/shared/game/CatanGame.java
@@ -16,17 +16,6 @@ public class CatanGame
 	/**
 	 * an array containing the players for the game. 
 	 */
-<<<<<<< Updated upstream
-	ArrayList<Player>myplayers=new ArrayList();
-	/**
-	 * the map for the game. 
-	 */
-	CatanMap mymap=new CatanMap(0, null);
-	/**
-	 * the chat system
-	 */
-	Chat mychat=new Chat();
-=======
 ArrayList<Player>myplayers=new ArrayList();
 /**
  * the map for the game. 
@@ -53,25 +42,7 @@ return true;
  */
 void startGame()
 {
->>>>>>> Stashed changes
 	
-	/**
-	 *  a function to see if we can start the game
-	 *  @exception throws exception if not able to start the game for anything that would prevent from starting
-	 *  such as not enough players, invalid Map,  Internet problems,  Server failure etc. 
-	 *  @custom.mytag1 post:return true if we can start the game. 
-	 * @return
-	 */
-	boolean canStartGame()
-	{
-	return true;	
-	}
-	/**
-	 * a function that starts the game nothing too fancy.   
-	 */
-	void startGame()
-	{
-		
-	}
+}
 
 }
