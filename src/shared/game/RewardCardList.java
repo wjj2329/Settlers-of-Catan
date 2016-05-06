78a0126973add6cbf9bb641cddbfa868c6865fb7
wjj2329 <wjj2329@gmail.com>
2016-05-05 20:01:45 -0600

    Reward Cards updated

--- src/shared/game/RewardCardList.java
+++ src/shared/game/RewardCardList.java
@@ -11,18 +11,36 @@ public class RewardCardList
 	 * We receive this as an integer from the JSON file.
 	 * However, we may create a LargestArmyCard class. 
 	 */
-	private int largestArmy = 0;
-	
+	private int largestArmySize = 0;
 	/**
 	 * Same as above for longestRoad.
 	 */
-	private int longestRoad = 0;
-	
+	private int longestRoadSize = 0;
 	/**
 	 * Constructor for RewardCardList
 	 */
 	public RewardCardList()
 	{
 		
+	}
+	/**
+	 * @custom.mytag1 pre: longestRoadSize is not negative and is not larger then there
+	 * are number of road pieces
+	 * @custom.mytag2 post:longestRoadSize is updated. 
+	 * @param longestRoadSize
+	 */
+	private void updatelongestroad(int longestRoadSize)
+	{
+		this.longestRoadSize=longestRoadSize;
+	}
+	/**
+	 * @custom.mytag1 pre: largestArmySize is not negative and is not larger then there
+	 * are number of Knight Cards
+	 * @custom.mytag2 post:largestArmySize is updated. 
+	 * @param longestRoadSize
+	 */
+	private void updatelargestarmy(int largestArmySize)
+	{
+		this.largestArmySize=largestArmySize;
 	}
 }
