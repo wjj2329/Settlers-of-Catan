693ee76e92ac0b62126cb5b1733cced26f70c3c2
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-05 21:07:17 -0600

    moo

--- src/client/model/MessageList.java
+++ src/client/model/MessageList.java
@@ -1,20 +1,25 @@
 package client.model;
-/**
-*A class that keeps a container of Message Line objects.
- */
+
 import java.util.ArrayList;
 
+/**
+* MessageList: A class that keeps a container of Message Line objects.
+*/
 public class MessageList
 {
-ArrayList<MessageLine>lines;
-
-/**
- * @param message  Not null string that contains a message
- * @param line Not null line that contains source of the said message.  
- * @exception throws invalidLocationException if line doesn't exist as a location
- */
-public void addtoList(String message, String line)
-{
-
-}
+	/**
+	 * All the chat lines and game history log messages.
+	 */
+	ArrayList<MessageLine>lines;
+	
+	/**
+	 * @param message  Not null string that contains a message
+	 * @param line Not null line that contains source of the said message.  
+	 * @exception throws invalidLocationException if line doesn't exist as a location
+	 * @custom.mytag1 pre: message and line must not be null!
+	 */
+	public void addtoList(String message, String line)
+	{
+		
+	}
 }
--- src/client/model/Model.java
+++ src/client/model/Model.java
@@ -1,47 +1,69 @@
 package client.model;
-/**
- * The client that handles talking with the server
- */
+
 import java.util.ArrayList;
 
 import shared.game.Bank;
 import shared.game.player.Player;
-import shared.game.map.Map;
+//import shared.game.map.Map;
 
+/**
+ * Model: The client that handles talking with the server.
+ */
 public class Model 
 {
-public Model()
-{
+	/**
+	 * ArrayList of players in the game.
+	 */
+	ArrayList<Player> myplayers=new ArrayList<>();
 	
-}
-/**
-*
-* */
-public void createGame()
-{
+	/**
+	 * Model Constructor
+	 */
+	public Model()
+	{
+		
+	}
 	
-}
-
-public void listGames()
-{
+	/**
+	* CreateGame function: Creates a new game. 
+	**/
+	public void createGame()
+	{
+		
+	}
 	
-}
-public void joinGame()
-{
+	/**
+	 * ListGames: Lists all the games that currently exist. 
+	 */
+	public void listGames()
+	{
+		
+	}
 	
-}
-public void serializeModel()
-{
+	/**
+	 * JoinGame: The player can join a game. Games must have 4 players in order 
+	 * to start!
+	 */
+	public void joinGame()
+	{
+		
+	}
 	
-}
-public void updatFromJSON()
-{
+	/**
+	 * SerializeModel: Serializes all our data to JSON so that the server can have
+	 * the data.
+	 */
+	public void serializeModel()
+	{
+		
+	}
 	
-}
-
-ArrayList<Player> myplayers=new ArrayList();
-
-
-
-
+	/**
+	 * updateFromJSON: The opposite of SerializeModel. Reads JSON data and 
+	 * puts it into the model. 
+	 */
+	public void updateFromJSON()
+	{
+		
+	}
 }
