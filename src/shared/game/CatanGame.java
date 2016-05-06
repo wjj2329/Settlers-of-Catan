7c9122c40b0f30779963f121e75c1278b836e4db
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-06 10:00:34 -0600

    thou shalt indent thy code.

--- src/shared/game/CatanGame.java
+++ src/shared/game/CatanGame.java
@@ -16,33 +16,33 @@ public class CatanGame
 	/**
 	 * an array containing the players for the game. 
 	 */
-ArrayList<Player>myplayers=new ArrayList();
-/**
- * the map for the game. 
- */
-CatanMap mymap=new CatanMap(0, null);
-/**
- * the chat system
- */
-Chat mychat=new Chat();
-
-/**
- *  a function to see if we can start the game
- *  @exception throws exception if not able to start the game for anything that would prevent from starting
- *  such as not enough players, invalid Map,  Internet problems,  Server failure etc. 
- *  @custom.mytag1 post:return true if we can start the game. 
- * @return
- */
-boolean canStartGame()
-{
-return true;	
-}
-/**
- * a function that starts the game nothing too fancy.   
- */
-void startGame()
-{
+	ArrayList<Player>myplayers=new ArrayList();
+	/**
+	 * the map for the game. 
+	 */
+	CatanMap mymap=new CatanMap(0, null);
+	/**
+	 * the chat system
+	 */
+	Chat mychat=new Chat();
 	
-}
+	/**
+	 *  a function to see if we can start the game
+	 *  @exception throws exception if not able to start the game for anything that would prevent from starting
+	 *  such as not enough players, invalid Map,  Internet problems,  Server failure etc. 
+	 *  @custom.mytag1 post:return true if we can start the game. 
+	 * @return
+	 */
+	boolean canStartGame()
+	{
+	return true;	
+	}
+	/**
+	 * a function that starts the game nothing too fancy.   
+	 */
+	void startGame()
+	{
+		
+	}
 
 }
