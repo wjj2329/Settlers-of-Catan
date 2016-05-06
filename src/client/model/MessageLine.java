47253b7584a0116b9b708b00d7ca256c1d7e117f
wjj2329 <wjj2329@gmail.com>
2016-05-05 18:00:06 -0600

    updating some client stuff

--- src/client/model/MessageLine.java
+++ src/client/model/MessageLine.java
@@ -1,8 +1,16 @@
 package client.model;
 
+/**
+* a simple class to keep track of messages from the players/server one line at a time
+ */
 public class MessageLine 
 {
 	String message;
 	String source;
+	public MessageLine(String message, String source)
+	{
+		this.message=message;
+		this.source=source;
+	}
 
 }
--- src/client/model/MessageList.java
+++ src/client/model/MessageList.java
@@ -1,8 +1,20 @@
 package client.model;
-
+/**
+*A class that keeps a container of Message Line objects.
+ */
 import java.util.ArrayList;
 
 public class MessageList
 {
 ArrayList<MessageLine>lines;
+
+/**
+ * @param message  Not null string that contains a message
+ * @param line Not null line that contains source of the said message.  
+ * @exception throws invalidLocationException if line doesn't exist as a location
+ */
+public void addtoList(String message, String line)
+{
+
+}
 }
