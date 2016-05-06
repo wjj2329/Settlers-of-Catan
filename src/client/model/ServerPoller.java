b1b2c6e33a3f69a70ba1b6174034d781f89f835b
wjj2329 <wjj2329@gmail.com>
2016-05-05 19:41:20 -0600

    server poller

--- src/client/model/ServerPoller.java
+++ src/client/model/ServerPoller.java
@@ -1,5 +1,14 @@
 package client.model;
 
-public class ServerPoller {
-
+public class ServerPoller 
+{
+String serverhost;
+String serverport;
+String urlprefix;
+void send(String urlpath, String data)
+{
+	
+}		
+	
 }
+
