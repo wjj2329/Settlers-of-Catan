2cb78ce1625640bd0c37480d2eb5a9454ae01889
matheusap <matheusavpacheco@gmail.com>
2016-05-05 22:49:44 -0600

    Now we have a bank

---
+++ src/client/model/Bank.java
@@ -0,0 +1,41 @@
+package model;
+
+/**
+ * Bank: Defines the Bank, where all extra cards are kept
+ */
+public class Bank
+{
+	private ResourceList cardslist;
+	
+	/**
+	 * Bank constructor
+	 */
+	public Bank()
+	{
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * hasCardAvailable: Checks to see if the bank has any of
+	 * the requested card available. Returns resulting boolean.
+	 */
+	public boolean hasCardAvailable()
+	{
+		return false;
+	}
+	/**
+	 * @return the cardslist
+	 */
+	public ResourceList getCardslist()
+	{
+		return cardslist;
+	}
+	/**
+	 * @param cardslist the cardslist to set
+	 */
+	public void setCardslist(ResourceList cardslist)
+	{
+		this.cardslist = cardslist;
+	}
+	
+	
+}
