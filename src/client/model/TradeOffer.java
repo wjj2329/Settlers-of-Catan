2ac843c77370fd5ccc2e062ab8212d352f84db62
wjj2329 <wjj2329@gmail.com>
2016-05-05 21:27:19 -0600

    gfj dhfhjdsjhkb kludsg

--- src/client/model/TradeOffer.java
+++ src/client/model/TradeOffer.java
@@ -4,22 +4,44 @@ import java.util.ArrayList;
 
 import javax.smartcardio.Card;
 
+import shared.game.player.Player;
+
+/**
+ * 
+ * A class that handles trade offers
+ *
+ */
 public class TradeOffer 
 {
 	int sender;
 	int receiver;
 	ArrayList<Card>offer=new ArrayList<Card>();
 	
-	boolean canTrade()
+	/**
+	 * @exception throws exception if player is Invalid/null
+	 * @custom.mytag1 pre: player is not null player is valid.  
+	 * @custom.mytag2 post:returns true if player can trade and false if can't
+	 * @return
+	 */
+	boolean canTrade(Player player)
 	{
 		return true;
 	}
 	
+	/**
+	 * @exception throws exception if player is Invalid/null
+	 * @custom.mytag1 pre: player is not null player is valid.  
+	 * @custom.mytag2 post:returns true if player can trade and false if can't
+	 * @param playerid
+	 */
 	void offerTrade(int playerid)
 	{
 		return;
 	}
 	
+	/**
+	 * makes trade 
+	 */
 	Card maketrade()
 	{
 		Card mycard=null;
