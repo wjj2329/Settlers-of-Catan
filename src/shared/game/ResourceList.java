a73d1916f3aacec9cd4fa3015f54015296cf23e9
wjj2329 <wjj2329@gmail.com>
2016-05-05 20:04:15 -0600

    Resource List!

--- src/shared/game/ResourceList.java
+++ src/shared/game/ResourceList.java
@@ -27,4 +27,44 @@ public class ResourceList
 	{
 		
 	}
+
+	public int getBrick() {
+		return brick;
+	}
+
+	public void setBrick(int brick) {
+		this.brick = brick;
+	}
+
+	public int getOre() {
+		return ore;
+	}
+
+	public void setOre(int ore) {
+		this.ore = ore;
+	}
+
+	public int getSheep() {
+		return sheep;
+	}
+
+	public void setSheep(int sheep) {
+		this.sheep = sheep;
+	}
+
+	public int getWheat() {
+		return wheat;
+	}
+
+	public void setWheat(int wheat) {
+		this.wheat = wheat;
+	}
+
+	public int getWood() {
+		return wood;
+	}
+
+	public void setWood(int wood) {
+		this.wood = wood;
+	}
 }
