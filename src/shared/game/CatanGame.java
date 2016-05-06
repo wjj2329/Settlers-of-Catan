8868f783a3fd826815933addc48df9c3dd781ead
wjj2329 <wjj2329@gmail.com>
2016-05-05 20:49:14 -0600

    catan game is beautiful

--- src/shared/game/CatanGame.java
+++ src/shared/game/CatanGame.java
@@ -5,14 +5,41 @@ import java.util.ArrayList;
 import shared.chat.Chat;
 import shared.game.map.CatanMap;
 import shared.game.player.Player;
+/**
+ * Catan Game object so that we can have a game accessible to be modified. 
+ */
+
 
 public class CatanGame 
 {
 	
+	/**
+	 * an array containing the players for the game. 
+	 */
 ArrayList<Player>myplayers=new ArrayList();
+/**
+ * the map for the game. 
+ */
 CatanMap mymap=new CatanMap(0, null);
+/**
+ * the chat system
+ */
 Chat mychat=new Chat();
 
+/**
+ *  a function to see if we can start the game
+ *  @exception throws exception if not able to start the game for anything that would prevent from starting
+ *  such as not enough players, invalid Map,  Internet problems,  Server failure etc. 
+ *  @custom.mytag1 post:return true if we can start the game. 
+ * @return
+ */
+boolean canStartGame()
+{
+return true;	
+}
+/**
+ * a function that starts the game nothing too fancy.   
+ */
 void startGame()
 {
 	
