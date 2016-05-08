0d07d6c6b3267b8bf6cb59282de5739ad0004bf3
wjj2329 <wjj2329@gmail.com>
2016-05-07 23:41:00 -0600

    Bank test

--- src/testpckg/CanBankGiveCard.java
+++ src/testpckg/CanBankGiveCard.java
@@ -16,13 +16,12 @@ public class CanBankGiveCard {
 
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
-
+	Bank mybank1=new Bank();
+	Bank mybank2=new Bank();
+	Bank mybank3=new Bank();
 	@Before
 	public void setUp()throws Exception
 	{
-		Bank mybank1=new Bank();
-		Bank mybank2=new Bank();
-		Bank mybank3=new Bank();
 		ResourceList mylist=new ResourceList();
 		mylist.setBrick(4);
 		mylist.setOre(3);
@@ -30,18 +29,8 @@ public class CanBankGiveCard {
 		mylist.setWheat(10);
 		mylist.setWood(0);
 		mybank1.setCardslist(mylist);
-		assertTrue(mybank1.CanBankGiveCard(ResourceType.BRICK));
-		assertFalse(mybank1.CanBankGiveCard(ResourceType.WOOD));
-		assertTrue(mybank1.CanBankGiveCard(ResourceType.WHEAT));
-		assertTrue(mybank1.CanBankGiveCard(ResourceType.SHEEP));
-		assertTrue(mybank1.CanBankGiveCard(ResourceType.ORE));
 		mybank2.setCardslist(0, 0 , 0 , 50, 25);
-		assertFalse(mybank2.CanBankGiveCard(ResourceType.BRICK));
-		assertTrue(mybank2.CanBankGiveCard(ResourceType.WOOD));
-		assertTrue(mybank2.CanBankGiveCard(ResourceType.ORE));
-		assertFalse(mybank2.CanBankGiveCard(ResourceType.WHEAT));
-		assertFalse(mybank2.CanBankGiveCard(ResourceType.SHEEP));
-		exception.expect(IndexOutOfBoundsException.class);
+		mybank3.setCardslist(-1, -9, -1 , -1, -1);
 	}
 
 	@After
@@ -53,6 +42,18 @@ public class CanBankGiveCard {
 	public void test()  throws Exception
 	{
 		assertTrue(true);
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.BRICK));
+		assertFalse(mybank1.CanBankGiveCard(ResourceType.WOOD));
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.WHEAT));
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.SHEEP));
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.ORE));
+		assertFalse(mybank2.CanBankGiveCard(ResourceType.BRICK));
+		assertTrue(mybank2.CanBankGiveCard(ResourceType.WOOD));
+		assertTrue(mybank2.CanBankGiveCard(ResourceType.ORE));
+		assertFalse(mybank2.CanBankGiveCard(ResourceType.WHEAT));
+		assertFalse(mybank2.CanBankGiveCard(ResourceType.SHEEP));
+		exception.expect(Exception.class);
+		mybank3.CanBankGiveCard(ResourceType.BRICK);
 	}
 
 }
