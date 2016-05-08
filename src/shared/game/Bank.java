c2a1b91b88ab610ad477c0d13deb5003f940c585
wjj2329 <wjj2329@gmail.com>
2016-05-07 22:05:00 -0600

    Bank test

--- src/shared/game/Bank.java
+++ src/shared/game/Bank.java
@@ -1,5 +1,6 @@
 package shared.game;
 
+import shared.definitions.ResourceType;
 import shared.game.Card;
 //import shared.game.PlayerNotFoundException;
 import shared.game.ResourceList;
@@ -9,7 +10,7 @@ import shared.game.ResourceList;
  */
 public class Bank
 {
-	private ResourceList cardslist;
+	private ResourceList cardslist=new ResourceList();
 	
 	/**
 	 * Bank constructor
@@ -22,8 +23,86 @@ public class Bank
 	 * hasCardAvailable: Checks to see if the bank has any of
 	 * the requested card available. Returns resulting boolean.
 	 */
-	public boolean CanBankGiveCard()
+	public boolean CanBankGiveCard(ResourceType mytype) throws Exception
 	{
+		switch(mytype)
+		{
+			case WOOD:
+			{
+				if(cardslist.getWood()<0)
+				{
+					Exception e=new Exception();
+					e.printStackTrace();
+					throw e;
+				}
+				if(cardslist.getWood()>0)
+				{
+					return true;
+				}
+				else return false;
+			}
+			case BRICK:
+			{
+				if(cardslist.getBrick()<0)
+				{
+					Exception e = new Exception();
+					e.printStackTrace();
+					throw e;
+				}
+				if(cardslist.getBrick()>0)
+				{
+					return true;
+				}
+				else return false;
+			}
+			case WHEAT:
+			{
+				if(cardslist.getWheat()<0)
+				{
+					Exception e = new Exception();
+					e.printStackTrace();
+					throw e;
+
+				}
+				if(cardslist.getWheat()>0)
+				{
+					return true;
+				}
+				else return false;
+			}
+			case SHEEP:
+			{
+				if(cardslist.getSheep()<0)
+				{
+					Exception e = new Exception();
+					e.printStackTrace();
+					throw e;
+				}
+				if(cardslist.getSheep()>0)
+				{
+					return true;
+				}
+				else
+					return false;
+
+			}
+			case ORE:
+			{
+				if(cardslist.getOre()<0)
+				{
+					Exception e = new Exception();
+					e.printStackTrace();
+					throw e;
+				}
+				if(cardslist.getOre()>0)
+				{
+					return true;
+				}
+				else
+					return false;
+
+			}
+		}
 		return false;
 	}
 	/**
@@ -39,6 +118,15 @@ public class Bank
 	public void setCardslist(ResourceList cardslist)
 	{
 		this.cardslist = cardslist;
+	}
+
+	public void setCardslist(int brick, int wheat, int sheep, int wood, int ore)
+	{
+		cardslist.setBrick(brick);
+		cardslist.setWheat(wheat);
+		cardslist.setSheep(sheep);
+		cardslist.setWood(wood);
+		cardslist.setOre(ore);
 	}
 	
 	/**
--- src/testpckg/CanBankGiveCard.java
+++ src/testpckg/CanBankGiveCard.java
@@ -4,22 +4,44 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import shared.definitions.ResourceType;
 import shared.game.Bank;
 import shared.game.ResourceList;
 
 public class CanBankGiveCard {
 
-	Bank mybank1=new Bank();
-	ResourceList mylist=new ResourceList();
+
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+
 	@Before
 	public void setUp()throws Exception
 	{
+		Bank mybank1=new Bank();
+		Bank mybank2=new Bank();
+		Bank mybank3=new Bank();
+		ResourceList mylist=new ResourceList();
 		mylist.setBrick(4);
 		mylist.setOre(3);
 		mylist.setSheep(2);
 		mylist.setWheat(10);
 		mylist.setWood(0);
+		mybank1.setCardslist(mylist);
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.BRICK));
+		assertFalse(mybank1.CanBankGiveCard(ResourceType.WOOD));
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.WHEAT));
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.SHEEP));
+		assertTrue(mybank1.CanBankGiveCard(ResourceType.ORE));
+		mybank2.setCardslist(0, 0 , 0 , 50, 25);
+		assertFalse(mybank2.CanBankGiveCard(ResourceType.BRICK));
+		assertTrue(mybank2.CanBankGiveCard(ResourceType.WOOD));
+		assertTrue(mybank2.CanBankGiveCard(ResourceType.ORE));
+		assertFalse(mybank2.CanBankGiveCard(ResourceType.WHEAT));
+		assertFalse(mybank2.CanBankGiveCard(ResourceType.SHEEP));
+		exception.expect(IndexOutOfBoundsException.class);
 	}
 
 	@After
