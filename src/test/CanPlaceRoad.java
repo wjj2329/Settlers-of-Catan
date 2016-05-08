9abb5e0e23021ec292b109d77f6dad40f03f6463
wjj2329 <wjj2329@gmail.com>
2016-05-07 18:51:16 -0600

    Test package made

---
+++ src/test/CanPlaceRoad.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import shared.game.map.CatanMap;
+
+public class CanPlaceRoad 
+{
+	CatanMap mymap=new CatanMap(0, null);
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+}
