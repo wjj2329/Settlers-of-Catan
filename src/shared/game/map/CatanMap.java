be64bcc345ff32bc10ec5b54653352783c8109c6
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-09 22:16:56 -0600

    the arraylist is now a map

--- src/shared/game/map/CatanMap.java
+++ src/shared/game/map/CatanMap.java
@@ -1,6 +1,8 @@
 package shared.game.map;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import client.main.Catan;
 import shared.definitions.HexType;
@@ -25,7 +27,7 @@ public class CatanMap
 	 * hexes: List of all the hexes in the map.
 	 * This will stay the same throughout the game.
 	 */
-	private ArrayList<Hex> hexes = new ArrayList<>();
+	private Map<HexLocation, Hex> hexes = new HashMap<>();
 
 	/**
 	 * ports: List of all the ports on the map.
@@ -86,7 +88,7 @@ public class CatanMap
 		this.desertHexLoc=desertHexLoc;
 	}
 
-	public CatanMap(ArrayList<Hex> hexes, int radius)
+	public CatanMap(Map<HexLocation, Hex> hexes, int radius)
 	{
 		this.hexes=hexes;
 		this.radius=radius;
@@ -95,50 +97,95 @@ public class CatanMap
 	public CatanMap(int radius)
 	{
 		this.radius = radius;
-		//this is the way the map will look by default if shuffling is not enabled. 
-		hexes.add(new Hex(new HexLocation(-3, 2), HexType.WATER, new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-3, 1), HexType.WATER, new NumberToken(0), PortType.WOOD));
-		hexes.add(new Hex(new HexLocation(-3, 0), HexType.WATER, new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-3, -1), HexType.WATER, new NumberToken(0), PortType.THREE));
+		//this is the way the map will look by default if shuffling is not enabled.
+		HexLocation hexLoc1 = new HexLocation(-3, 2);
+		HexLocation hexLoc2 = new HexLocation(-3, 1);
+		HexLocation hexLoc3 = new HexLocation(-3, 0);
+		HexLocation hexLoc4 = new HexLocation(-3, -1);
 
-		hexes.add(new Hex(new HexLocation(-2, -2), HexType.WATER, new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-2, -1), HexType.ORE, new NumberToken(5), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-2, 0), HexType.WHEAT, new NumberToken(2), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-2,1), HexType.WOOD, new NumberToken(6), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-2, 2), HexType.WATER, new NumberToken(0), PortType.BRICK));
+		HexLocation hexLoc5 = new HexLocation(-2, -2);
+		HexLocation hexLoc6 = new HexLocation(-2, -1);
+		HexLocation hexLoc7 = new HexLocation(-2, 0);
+		HexLocation hexLoc8 = new HexLocation(-2, 1);
+		HexLocation hexLoc9 = new HexLocation(-2, 2);
 
-		hexes.add(new Hex(new HexLocation(-1,3), HexType.WATER, new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-1, 2), HexType.ORE, new NumberToken(3), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-1, 1), HexType.SHEEP, new NumberToken(9), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-1, 0), HexType.SHEEP, new NumberToken(10), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-1, -1), HexType.BRICK, new NumberToken(8), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(-1, -2), HexType.WATER, new NumberToken(0), PortType.WHEAT));
+		HexLocation hexLoc10 = new HexLocation(-1, 3);
+		HexLocation hexLoc11 = new HexLocation(-1, 2);
+		HexLocation hexLoc12 = new HexLocation(-1, 1);
+		HexLocation hexLoc13 = new HexLocation(-1, 0);
+		HexLocation hexLoc14 = new HexLocation(-1, -1);
+		HexLocation hexLoc15 = new HexLocation(-1, -2);
 
-		hexes.add(new Hex(new HexLocation(0,3), HexType.WATER,new NumberToken(0), PortType.THREE));
-		hexes.add(new Hex(new HexLocation(0, 2), HexType.WHEAT, new NumberToken(8), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(0, 1), HexType.WOOD, new NumberToken(4), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(0, 0), HexType.WHEAT, new NumberToken(11), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(0, -1), HexType.WOOD, new NumberToken(3), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(0, -2),HexType.DESERT,new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(0, -3), HexType.WATER, new NumberToken(0), PortType.BLANK));
+		HexLocation hexLoc16 = new HexLocation(0,3);
+		HexLocation hexLoc17 =  new HexLocation(0, 2);
+		HexLocation hexLoc18 = new HexLocation(0, 1);
+		HexLocation hexLoc19 = new HexLocation(0, 0);
+		HexLocation hexLoc20 = new HexLocation(0, -1);
+		HexLocation hexLoc21 = new HexLocation(0, -2);
+		HexLocation hexLoc22 = new HexLocation(0, -3);
 
-		hexes.add(new Hex(new HexLocation(1, -3), HexType.WATER, new NumberToken(0), PortType.ORE));
-		hexes.add(new Hex(new HexLocation(1, -2), HexType.BRICK, new NumberToken(4), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(1, -1), HexType.ORE, new NumberToken(9), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(1, 0), HexType.BRICK, new NumberToken(5), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(1, 1), HexType.SHEEP, new NumberToken(10), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(1, 2), HexType.WATER, new NumberToken(0), PortType.BLANK));
+		HexLocation hexLoc23 = new HexLocation(1, -3);
+		HexLocation hexLoc24 = new HexLocation(1, -2);
+		HexLocation hexLoc25 = new HexLocation(1, -1);
+		HexLocation hexLoc26 = new HexLocation(1, 0);
+		HexLocation hexLoc27 = new HexLocation(1, 1);
+		HexLocation hexLoc28 = new HexLocation(1, 2);
 
-		hexes.add(new Hex(new HexLocation(2, -2), HexType.WATER, new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(2, -1), HexType.WOOD, new NumberToken(11), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(2,0),HexType.SHEEP, new NumberToken(12), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(2,1),HexType.WHEAT, new NumberToken(6), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(2, 2), HexType.WATER,new NumberToken(0), PortType.THREE));
+		HexLocation hexLoc29 = new HexLocation(2, -2);
+		HexLocation hexLoc30 = new HexLocation(2, -1);
+		HexLocation hexLoc31 = new HexLocation(2,0);
+		HexLocation hexLoc32 = new HexLocation(2,1);
+		HexLocation hexLoc33 = new HexLocation(2, 2);
 
-		hexes.add(new Hex(new HexLocation(3, 2), HexType.WATER, new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(3, 1), HexType.WATER, new NumberToken(0), PortType.SHEEP));
-		hexes.add(new Hex(new HexLocation(3, 0), HexType.WATER, new NumberToken(0), PortType.BLANK));
-		hexes.add(new Hex(new HexLocation(3, -1), HexType.WATER, new NumberToken(0), PortType.THREE));
+		HexLocation hexLoc34 = new HexLocation(3, 2);
+		HexLocation hexLoc35 = new HexLocation(3, 1);
+		HexLocation hexLoc36 = new HexLocation(3, 0);
+		HexLocation hexLoc37 = new HexLocation(3, -1);
+
+		hexes.put(hexLoc1, new Hex(hexLoc1, HexType.WATER, new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc2, new Hex(hexLoc2, HexType.WATER, new NumberToken(0), PortType.WOOD));
+		hexes.put(hexLoc3, new Hex(hexLoc3, HexType.WATER, new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc4, new Hex(hexLoc4, HexType.WATER, new NumberToken(0), PortType.THREE));
+
+		hexes.put(hexLoc5, new Hex(hexLoc5, HexType.WATER, new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc6, new Hex(hexLoc6, HexType.ORE, new NumberToken(5), PortType.BLANK));
+		hexes.put(hexLoc7, new Hex(hexLoc7, HexType.WHEAT, new NumberToken(2), PortType.BLANK));
+		hexes.put(hexLoc8, new Hex(hexLoc8, HexType.WOOD, new NumberToken(6), PortType.BLANK));
+		hexes.put(hexLoc9, new Hex(hexLoc9, HexType.WATER, new NumberToken(0), PortType.BRICK));
+
+		hexes.put(hexLoc10, new Hex(hexLoc10, HexType.WATER, new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc11, new Hex(hexLoc11, HexType.ORE, new NumberToken(3), PortType.BLANK));
+		hexes.put(hexLoc12, new Hex(hexLoc12, HexType.SHEEP, new NumberToken(9), PortType.BLANK));
+		hexes.put(hexLoc13, new Hex(hexLoc13, HexType.SHEEP, new NumberToken(10), PortType.BLANK));
+		hexes.put(hexLoc14, new Hex(hexLoc14, HexType.BRICK, new NumberToken(8), PortType.BLANK));
+		hexes.put(hexLoc15, new Hex(hexLoc15, HexType.WATER, new NumberToken(0), PortType.WHEAT));
+
+		hexes.put(hexLoc16, new Hex(hexLoc16, HexType.WATER,new NumberToken(0), PortType.THREE));
+		hexes.put(hexLoc17, new Hex(hexLoc17, HexType.WHEAT, new NumberToken(8), PortType.BLANK));
+		hexes.put(hexLoc18, new Hex(hexLoc18, HexType.WOOD, new NumberToken(4), PortType.BLANK));
+		hexes.put(hexLoc19, new Hex(hexLoc19, HexType.WHEAT, new NumberToken(11), PortType.BLANK));
+		hexes.put(hexLoc20, new Hex(hexLoc20, HexType.WOOD, new NumberToken(3), PortType.BLANK));
+		hexes.put(hexLoc21, new Hex(hexLoc21,HexType.DESERT,new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc22, new Hex(hexLoc22, HexType.WATER, new NumberToken(0), PortType.BLANK));
+
+		hexes.put(hexLoc23, new Hex(hexLoc23, HexType.WATER, new NumberToken(0), PortType.ORE));
+		hexes.put(hexLoc24, new Hex(hexLoc24, HexType.BRICK, new NumberToken(4), PortType.BLANK));
+		hexes.put(hexLoc25, new Hex(hexLoc25, HexType.ORE, new NumberToken(9), PortType.BLANK));
+		hexes.put(hexLoc26, new Hex(hexLoc26, HexType.BRICK, new NumberToken(5), PortType.BLANK));
+		hexes.put(hexLoc27, new Hex(hexLoc27, HexType.SHEEP, new NumberToken(10), PortType.BLANK));
+		hexes.put(hexLoc28, new Hex(hexLoc28, HexType.WATER, new NumberToken(0), PortType.BLANK));
+
+		// i am here currently
+		hexes.put(hexLoc29, new Hex(hexLoc29, HexType.WATER, new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc30, new Hex(hexLoc30, HexType.WOOD, new NumberToken(11), PortType.BLANK));
+		hexes.put(hexLoc31, new Hex(hexLoc31,HexType.SHEEP, new NumberToken(12), PortType.BLANK));
+		hexes.put(hexLoc32, new Hex(hexLoc32,HexType.WHEAT, new NumberToken(6), PortType.BLANK));
+		hexes.put(hexLoc33, new Hex(hexLoc33, HexType.WATER,new NumberToken(0), PortType.THREE));
+
+		hexes.put(hexLoc34, new Hex(hexLoc34, HexType.WATER, new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc35, new Hex(hexLoc35, HexType.WATER, new NumberToken(0), PortType.SHEEP));
+		hexes.put(hexLoc36, new Hex(hexLoc36, HexType.WATER, new NumberToken(0), PortType.BLANK));
+		hexes.put(hexLoc37, new Hex(hexLoc37, HexType.WATER, new NumberToken(0), PortType.THREE));
 
 	}
 	
@@ -182,11 +229,11 @@ public class CatanMap
 		this.cities = cities;
 	}
 
-	public ArrayList<Hex> getHexes() {
+	public Map<HexLocation, Hex> getHexes() {
 		return hexes;
 	}
 
-	public void setHexes(ArrayList<Hex> hexes) {
+	public void setHexes(Map<HexLocation, Hex> hexes) {
 		this.hexes = hexes;
 	}
 
