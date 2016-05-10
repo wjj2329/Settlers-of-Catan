fed12f4c548e42f0dc77a3d453ef075dbc8e3800
wjj2329 <wjj2329@gmail.com>
2016-05-09 18:27:05 -0600

    create

---
+++ src/testpckg/CanCreatePlayer.java
@@ -0,0 +1,18 @@
+package testpckg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CanCreatePlayer {
+
+	/*
+	hello
+	 */
+	@Test
+	public void test() {
+
+		fail("Not yet implemented");
+	}
+
+}
