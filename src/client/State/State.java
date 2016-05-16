184db3aea29809b0c05106f55b3133b8605dc9aa
guzmanz <zulguzman@yahoo.com>
2016-05-16 08:16:48 -0600

    guess it looks like dis

--- src/client/State/State.java
+++ src/client/State/State.java
@@ -2,64 +2,29 @@ package client.State;
 
 public enum State {
 
+
 	LoginState{
+		//player logins or registers 
+		
 		@Override
-		public void playSoldierCard() {
+		public void login() {
 
 		}
 
 		@Override
-		public void playRoadBuildingCard() {
+		public void register() {
 
 		}
 
-		@Override
-		public void playMonumentCard() {
-
-		}
-
-		@Override
-		public void playMonopolyCard() {
-
-		}
-
-		@Override
-		public void buildCity() {
-
-		}
-
-		@Override
-		public void buildSettlement() {
-
-		}
-
-		@Override
-		public void buildRoad() {
-
-		}
-
-		@Override
-		public void rollNumber() {
-
-		}
-
-		@Override
-		public void createGame() {
-
-		}
-
-		@Override
-		public void addAI() {
-
-		}
 
 		@Override
 		public String getState() {
-			return null;
+			return "LOGIN";
 		}
 	},
 	
 	GamePlayingState{
+		//player is actually playing the game.
 		@Override
 		public void playSoldierCard() {
 
@@ -99,48 +64,15 @@ public enum State {
 		public void rollNumber() {
 
 		}
-
-		@Override
-		public void createGame() {
-
-		}
-
-		@Override
-		public void addAI() {
-
-		}
-
+		
 		@Override
 		public String getState() {
-			return null;
+			return "GAMEPLAYING";
 		}
 	},
 	SetUpState{
-		@Override
-		public void playSoldierCard() {
-
-		}
-
-		@Override
-		public void playRoadBuildingCard() {
-
-		}
-
-		@Override
-		public void playMonumentCard() {
-
-		}
-
-		@Override
-		public void playMonopolyCard() {
-
-		}
-
-		@Override
-		public void buildCity() {
-
-		}
-
+		//First set up round player gets 2 free settlements and roads
+		
 		@Override
 		public void buildSettlement() {
 
@@ -152,70 +84,12 @@ public enum State {
 		}
 
 		@Override
-		public void rollNumber() {
-
-		}
-
-		@Override
-		public void createGame() {
-
-		}
-
-		@Override
-		public void addAI() {
-
-		}
-
-		@Override
 		public String getState() {
-			return null;
+			return "SETUP";
 		}
 	},
 	PlayerWaitingState{
-		@Override
-		public void playSoldierCard() {
-
-		}
-
-		@Override
-		public void playRoadBuildingCard() {
-
-		}
-
-		@Override
-		public void playMonumentCard() {
-
-		}
-
-		@Override
-		public void playMonopolyCard() {
-
-		}
-
-		@Override
-		public void buildCity() {
-
-		}
-
-		@Override
-		public void buildSettlement() {
-
-		}
-
-		@Override
-		public void buildRoad() {
-
-		}
-
-		@Override
-		public void rollNumber() {
-
-		}
-
-		@Override
-		public void createGame() {
-
-		}
+		//player waits for other players to join game can add AI player
 
 		@Override
 		public void addAI() {
@@ -224,79 +98,44 @@ public enum State {
 
 		@Override
 		public String getState() {
-			return null;
+			return "PLAYERWAITING";
 		}
 	},
 	JoinGameState{
+		//player can join or re-join games or create game
+		
 		@Override
-		public void playSoldierCard() {
+		public void joinGame() {
 
 		}
-
-		@Override
-		public void playRoadBuildingCard() {
-
-		}
-
-		@Override
-		public void playMonumentCard() {
-
-		}
-
-		@Override
-		public void playMonopolyCard() {
-
-		}
-
-		@Override
-		public void buildCity() {
-
-		}
-
-		@Override
-		public void buildSettlement() {
-
-		}
-
-		@Override
-		public void buildRoad() {
-
-		}
-
-		@Override
-		public void rollNumber() {
-
-		}
-
+		
 		@Override
 		public void createGame() {
 
 		}
-
-		@Override
-		public void addAI() {
-
-		}
-
+		
 		@Override
 		public String getState() {
-			return null;
+			return "JOINGAME";
 		}
 	};
 	
-	public abstract void playSoldierCard();
-	public abstract void playRoadBuildingCard();
-	public abstract void playMonumentCard();
-	public abstract void playMonopolyCard();
-	public abstract void buildCity();
-	public abstract void buildSettlement();
-	public abstract void buildRoad();
-	public abstract void rollNumber();
-	public abstract void createGame();
-	public abstract void addAI();
-	public abstract String getState();
 	
-	
-	
-	
+	//default methods do nothing.
+	public void login(){}
+	public void joinGame(){}
+	public void register(){}
+	public void playSoldierCard(){}
+	public void playRoadBuildingCard(){}
+	public void playMonumentCard(){}
+	public void playMonopolyCard(){}
+	public void buildCity(){}
+	public void buildSettlement(){}
+	public void buildRoad(){}
+	public void placeRobber(){}
+	public void rollNumber(){}
+	public void createGame(){}
+	public void addAI(){}
+	public String getState(){ return null; }
+
 }
