46eaf88cf8380583202d1f36862ac84f2c9ef08b
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-16 22:49:39 -0600

    i took out some debugging crap we didn't need

--- src/client/resources/ResourceBarController.java
+++ src/client/resources/ResourceBarController.java
@@ -35,13 +35,7 @@ public class ResourceBarController extends Controller implements IResourceBarCon
 	 */
 	public void setElementAction(ResourceBarElement element, IAction action) {
 
-		CatanGame.singleton.getCurrentPlayer().setResources(new ResourceList(5,5,5,5,5));
 		elementActions.put(element, action);
-		this.getView().setElementAmount(ResourceBarElement.BRICK, CatanGame.singleton.getCurrentPlayer().getResources().getBrick());
-		this.getView().setElementAmount(ResourceBarElement.WHEAT, CatanGame.singleton.getCurrentPlayer().getResources().getWheat());
-		this.getView().setElementAmount(ResourceBarElement.WOOD, CatanGame.singleton.getCurrentPlayer().getResources().getWood());
-		this.getView().setElementAmount(ResourceBarElement.SHEEP, CatanGame.singleton.getCurrentPlayer().getResources().getSheep());
-		this.getView().setElementAmount(ResourceBarElement.ORE,CatanGame.singleton.getCurrentPlayer().getResources().getOre());
 	}
 
 	@Override
