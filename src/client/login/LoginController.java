642fe0c78b8f298bc33cccdc7c0a99a091551587
wjj2329 <wjj2329@gmail.com>
2016-05-17 10:07:56 -0600

    Register User works

--- src/client/login/LoginController.java
+++ src/client/login/LoginController.java
@@ -93,7 +93,7 @@ public class LoginController extends Controller implements ILoginController, Obs
 		String username = getLoginView().getRegisterUsername();
 		String password = getLoginView().getRegisterPassword();
 		String passrepeat = getLoginView().getRegisterPasswordRepeat();
-		if(password.equals(passRepeat))
+		if(password.equals(passrepeat))
 		{
 			if(server.registerUser(username,password).getResponseCode()==HttpURLConnection.HTTP_OK)
 			{
