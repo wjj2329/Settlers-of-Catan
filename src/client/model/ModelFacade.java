885ef6028e5ca595736fdbea28efd5f6eeb3078c
wjj2329 <wjj2329@gmail.com>
2016-05-17 11:46:53 -0600

    Fixed importer

--- src/client/model/ModelFacade.java
+++ src/client/model/ModelFacade.java
@@ -297,6 +297,8 @@ public class ModelFacade implements Observable
 		}
 	}
 
+
+
 	private void loadMap(JSONObject map) throws JSONException
 	{
 		JSONArray hexes = map.getJSONArray("hexes");
@@ -321,10 +323,10 @@ public class ModelFacade implements Observable
 			String direction = obj.getString("direction");
 			EdgeDirection dir = getDirectionFromString(direction);
 			assert(dir != null);
-			//Port newPort = new Port(new HexLocation(location.getInt("x"), location.getInt("y")), dir,
-					//obj.getInt("ratio"));
-			//newPort.setType(getPortTypeFromString(resource));
-			//CatanGame.singleton.getMymap().getPorts().add(newPort);
+			Port newPort = new Port(new HexLocation(location.getInt("x"), location.getInt("y")), dir,
+					obj.getInt("ratio"),getPortTypeFromString(resource));//this is not going
+			newPort.setType(getPortTypeFromString(resource));
+			CatanGame.singleton.getMymap().getPorts().add(newPort);
 		}
 		JSONArray roads = map.getJSONArray("roads");
 		for (int i = 0; i < roads.length(); i++)
