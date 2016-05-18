300de28794655e36e4ba66e7727f2e67886ec75b
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-18 10:45:01 -0600

    blah

--- src/client/join/JoinGameController.java
+++ src/client/join/JoinGameController.java
@@ -22,119 +22,108 @@ import client.model.ModelFacade;
  */
 public class JoinGameController extends Controller implements IJoinGameController, Observer {
 
-	private INewGameView newGameView;
-	private ISelectColorView selectColorView;
-	private IMessageView messageView;
-	private IAction joinAction;
+    private INewGameView newGameView;
+    private ISelectColorView selectColorView;
+    private IMessageView messageView;
+    private IAction joinAction;
     private Timer timer;
     private GameInfo game = null;
     private boolean shouldShowGameList = true;
-    //private GameInfo[] lastList = new GameInfo[200];
-    private ArrayList<GameInfo> lastList = new ArrayList<>();
+    private GameInfo[] lastList = null;
     private ArrayList<CatanColor> colorsTaken = null;
     public static  String colorforit;
     public static int gameindex;
     public static int playerindexforit=0;
-    private int arrayIndexForLastList = 0;
-	//private GameInfo[] games;
-	
-	/**
-	 * JoinGameController constructor
-	 * 
-	 * @param view Join game view
-	 * @param newGameView New game view
-	 * @param selectColorView Select color view
-	 * @param messageView Message view (used to display error messages that occur while the user is joining a game)
-	 */
-	public JoinGameController(IJoinGameView view, INewGameView newGameView, 
-								ISelectColorView selectColorView, IMessageView messageView) {
+    //private GameInfo[] games;
 
-		super(view);
-        /*lastList = new GameInfo[200];
-        System.out.println("The INITIAL size of the array is " + lastList.length);
-        for (int i = 0; i < 200; i++)
-        {
-            lastList[i] = new GameInfo();
-        }
-        System.out.println("after the for loop, the size is " + lastList.length);*/
-		setNewGameView(newGameView);
-		setSelectColorView(selectColorView);
-		setMessageView(messageView);
-		ModelFacade.facace_currentgame.addObserver(this);
-	}
-	
-	public IJoinGameView getJoinGameView() {
-		
-		return (IJoinGameView)super.getView();
-	}
-	
-	/**
-	 * Returns the action to be executed when the user joins a game
-	 * 
-	 * @return The action to be executed when the user joins a game
-	 */
-	public IAction getJoinAction() {
-		
-		return joinAction;
-	}
+    /**
+     * JoinGameController constructor
+     *
+     * @param view Join game view
+     * @param newGameView New game view
+     * @param selectColorView Select color view
+     * @param messageView Message view (used to display error messages that occur while the user is joining a game)
+     */
+    public JoinGameController(IJoinGameView view, INewGameView newGameView,
+                              ISelectColorView selectColorView, IMessageView messageView) {
 
-	/**
-	 * Sets the action to be executed when the user joins a game
-	 * 
-	 * @param value The action to be executed when the user joins a game
-	 */
-	public void setJoinAction(IAction value) {	
-		
-		joinAction = value;
-	}
-	
-	public INewGameView getNewGameView() {
-		
-		return newGameView;
-	}
+        super(view);
 
-	public void setNewGameView(INewGameView newGameView) {
-		
-		this.newGameView = newGameView;
-	}
-	
-	public ISelectColorView getSelectColorView() {
-		
-		return selectColorView;
-	}
-	public void setSelectColorView(ISelectColorView selectColorView) {
-		
-		this.selectColorView = selectColorView;
-	}
-	
-	public IMessageView getMessageView() {
-		
-		return messageView;
-	}
-	public void setMessageView(IMessageView messageView) {
-		
-		this.messageView = messageView;
-	}
-	
-	private synchronized void refreshGameList()
-	{
+        setNewGameView(newGameView);
+        setSelectColorView(selectColorView);
+        setMessageView(messageView);
+        ModelFacade.facace_currentgame.addObserver(this);
+    }
+
+    public IJoinGameView getJoinGameView() {
+
+        return (IJoinGameView)super.getView();
+    }
+
+    /**
+     * Returns the action to be executed when the user joins a game
+     *
+     * @return The action to be executed when the user joins a game
+     */
+    public IAction getJoinAction() {
+
+        return joinAction;
+    }
+
+    /**
+     * Sets the action to be executed when the user joins a game
+     *
+     * @param value The action to be executed when the user joins a game
+     */
+    public void setJoinAction(IAction value) {
+
+        joinAction = value;
+    }
+
+    public INewGameView getNewGameView() {
+
+        return newGameView;
+    }
+
+    public void setNewGameView(INewGameView newGameView) {
+
+        this.newGameView = newGameView;
+    }
+
+    public ISelectColorView getSelectColorView() {
+
+        return selectColorView;
+    }
+    public void setSelectColorView(ISelectColorView selectColorView) {
+
+        this.selectColorView = selectColorView;
+    }
+
+    public IMessageView getMessageView() {
+
+        return messageView;
+    }
+    public void setMessageView(IMessageView messageView) {
+
+        this.messageView = messageView;
+    }
+
+    private synchronized void refreshGameList()
+    {
         ArrayList<CatanGame> gamesList = ModelFacade.facace_currentgame.getModel().listGames();
 
         if (gamesList == null)
         {
             return;
         }
-        //GameInfo[] games = new GameInfo[gamesList.size()];
-        ArrayList<GameInfo> games = new ArrayList<>();
+        GameInfo[] games = new GameInfo[gamesList.size()];
         GameInfo currentGame = null;
         int idx = 0;
         for(CatanGame gameChoice: gamesList)
         {
-            GameInfo newGameInfo = new GameInfo();
-            //newGameInfo = games.get(idx);// = new GameInfo();*/
-            newGameInfo.setId(gameChoice.getGameId());
-            newGameInfo.setTitle(gameChoice.getTitle());
-            games.add(newGameInfo);
+            games[idx] = new GameInfo();
+            games[idx].setId(gameChoice.getGameId());
+            games[idx].setTitle(gameChoice.getTitle());
             int playerIndex = 0;
             for(Player player: gameChoice.getMyplayers().values())
             {
@@ -143,24 +132,24 @@ public class JoinGameController extends Controller implements IJoinGameControlle
                 onePlayersInfo.setId(player.getPlayerID().getNumber());
                 onePlayersInfo.setName(player.getName());
                 onePlayersInfo.setPlayerIndex(playerIndex++);
-                games.get(idx).addPlayer(onePlayersInfo);
+                games[idx].addPlayer(onePlayersInfo);
             }
-            if(game != null && games.get(idx).getId() == game.getId())
+            if(game != null && games[idx].getId() == game.getId())
             {
-                currentGame = games.get(idx);
+                currentGame = games[idx];
             }
-            idx++;   
+            idx++;
         }
         if(currentGame != null)
         {
             startJoinGame(currentGame);
         }
 
-        /*if(Arrays.equals(games, lastListd))
+        if(Arrays.equals(games, lastList))
         {
             return;
-        }*/
-        
+        }
+
         lastList = games;
         PlayerInfo localPlayer = new PlayerInfo();
         localPlayer.setId(playerindexforit);
@@ -168,10 +157,10 @@ public class JoinGameController extends Controller implements IJoinGameControlle
         getJoinGameView().setGames(games, localPlayer);
         if(shouldShowGameList)
         {
-        	if(getJoinGameView().isModalShowing())
-        	{
-        		getJoinGameView().closeModal();
-        	}
+            if(getJoinGameView().isModalShowing())
+            {
+                getJoinGameView().closeModal();
+            }
             getJoinGameView().showModal();
         }
         else if(game != null)
@@ -185,14 +174,14 @@ public class JoinGameController extends Controller implements IJoinGameControlle
                         for(PlayerInfo pInfo: gameInList.getPlayers())
                             if(pInfo.getId() == localPlayer.getId())
                             {
-                            	if(getJoinGameView().isModalShowing())
-                            	{
-                            		getJoinGameView().closeModal();
-                            	}
-                            	if(getSelectColorView().isModalShowing())
-                            	{
-                            		getSelectColorView().closeModal();
-                            	}
+                                if(getJoinGameView().isModalShowing())
+                                {
+                                    getJoinGameView().closeModal();
+                                }
+                                if(getSelectColorView().isModalShowing())
+                                {
+                                    getSelectColorView().closeModal();
+                                }
                                 return;
                             }
                         cancelJoinGame();
@@ -205,16 +194,10 @@ public class JoinGameController extends Controller implements IJoinGameControlle
         }
     }
 
-	@Override
-	public void start() 
-	{
-        lastList = new GameInfo[200];
-        System.out.println("The STARTING size of the array is " + lastList.length);
-        for (int i = 0; i < 200; i++)
-        {
-            lastList[i] = new GameInfo();
-        }
-		getJoinGameView().showModal();
+    @Override
+    public void start()
+    {
+        getJoinGameView().showModal();
         TimerTask timerTask = new TimerTask()
         {
             @Override
@@ -225,58 +208,47 @@ public class JoinGameController extends Controller implements IJoinGameControlle
         };
         timer = new Timer();
         timer.schedule(timerTask, 0, 1500);
-	}
+    }
 
-	@Override
-	public void startCreateNewGame()
-	{
-    	if(getJoinGameView().isModalShowing())
-    	{
-    		getJoinGameView().closeModal();
-    	}
+    @Override
+    public void startCreateNewGame()
+    {
+        if(getJoinGameView().isModalShowing())
+        {
+            getJoinGameView().closeModal();
+        }
         shouldShowGameList = false;
         getNewGameView().setTitle("");
         getNewGameView().setRandomlyPlaceHexes(false);
         getNewGameView().setRandomlyPlaceNumbers(false);
         getNewGameView().setUseRandomPorts(false);
         getNewGameView().showModal();
-	}
+    }
 
-	@Override
-	public void cancelCreateNewGame()
-	{
-    	if(getNewGameView().isModalShowing())
-    	{
-    		getNewGameView().closeModal();
-    	}
+    @Override
+    public void cancelCreateNewGame()
+    {
+        if(getNewGameView().isModalShowing())
+        {
+            getNewGameView().closeModal();
+        }
         shouldShowGameList = true;
         getJoinGameView().showModal();
-	}
+    }
 
-	@Override
-	public void createNewGame() 
-	{
-        lastList = new GameInfo[200];
-        System.out.println("The CREATE NEW GAME size of the array is " + lastList.length);
-        for (int i = 0; i < 200; i++)
-        {
-            lastList[i] = new GameInfo();
-        }
-		//Establish game details
-		boolean randomlyPlaceHexes = getNewGameView().getRandomlyPlaceHexes();
+    @Override
+    public void createNewGame()
+    {
+        //Establish game details
+        boolean randomlyPlaceHexes = getNewGameView().getRandomlyPlaceHexes();
         boolean randomlyPlaceNumbers = getNewGameView().getRandomlyPlaceNumbers();
         String title = getNewGameView().getTitle();
         boolean randomPorts = getNewGameView().getUseRandomPorts();
         boolean validTitle = !(title == null || title.trim().equals(""));
-        GameInfo currentInfo = new GameInfo();
-        currentInfo.setTitle(title);
-        System.out.println("The array index IS: " + arrayIndexForLastList);
-        System.out.println("The size of the array is " + lastList.length);
-        lastList[arrayIndexForLastList] = currentInfo;
-        arrayIndexForLastList++;
+
 
         System.out.println("Size of lastList is " + lastList.length);
-        
+
         //Check title against other titles (needs to be unique)
         if(lastList != null)
             for(GameInfo info: lastList)
@@ -294,33 +266,33 @@ public class JoinGameController extends Controller implements IJoinGameControlle
             getMessageView().showModal();
             return;
         }
-        
+
         if(getNewGameView().isModalShowing())
         {
-        	getNewGameView().closeModal();
+            getNewGameView().closeModal();
         }
-		try
-		{
-			ModelFacade.facace_currentgame.getModel().createGame(randomlyPlaceNumbers, randomlyPlaceHexes, randomPorts, title);
-		} 
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-        
+        try
+        {
+            ModelFacade.facace_currentgame.getModel().createGame(randomlyPlaceNumbers, randomlyPlaceHexes, randomPorts, title);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
         getJoinGameView().showModal();
         shouldShowGameList = true;
         refreshGameList();
     }
 
     @Override
-    public void startJoinGame(GameInfo game) 
+    public void startJoinGame(GameInfo game)
     {
         this.game = game;
         shouldShowGameList = false;
         Collection<PlayerInfo> currentplayers = game.getPlayers();
         ArrayList<CatanColor> currentColorsTaken = new ArrayList<CatanColor>();
-        
+
         //What colors are already being used?
         for(PlayerInfo playerinfo: currentplayers)
         {
@@ -333,7 +305,7 @@ public class JoinGameController extends Controller implements IJoinGameControlle
         {
             return;
         }
-        
+
         //Color now assigned to client player
         colorsTaken = currentColorsTaken;
         for(CatanColor color: CatanColor.values())
@@ -344,18 +316,18 @@ public class JoinGameController extends Controller implements IJoinGameControlle
         {
             getSelectColorView().showModal();
         }
-	}
-    
-	@Override
-	public void cancelJoinGame() 
-	{
-	
-		getJoinGameView().closeModal();
-	}
+    }
 
-	@Override
-	public void joinGame(CatanColor color)
-	{
+    @Override
+    public void cancelJoinGame()
+    {
+
+        getJoinGameView().closeModal();
+    }
+
+    @Override
+    public void joinGame(CatanColor color)
+    {
         colorforit=color.toString();
         ArrayList<CatanGame> games = ModelFacade.facace_currentgame.getModel().listGames();
         for(CatanGame game: games)
@@ -366,9 +338,9 @@ public class JoinGameController extends Controller implements IJoinGameControlle
                 for(Player player: game.getMyplayers().values())
                 {
                     getSelectColorView().setColorEnabled(color, false);
-                    if(player.getColor().equals(color) && 
-                    		player.getPlayerIndex().getNumber() != 
-                    		ModelFacade.facace_currentgame.getLocalPlayer().getPlayerIndex().getNumber())
+                    if(player.getColor().equals(color) &&
+                            player.getPlayerIndex().getNumber() !=
+                                    ModelFacade.facace_currentgame.getLocalPlayer().getPlayerIndex().getNumber())
                     {
                         getMessageView().setMessage("Cannot join with that color.  Already taken.");
                         getMessageView().showModal();
@@ -379,24 +351,24 @@ public class JoinGameController extends Controller implements IJoinGameControlle
             }
         }
         ModelFacade.facace_currentgame.getModel().joinGame(ModelFacade.facace_currentgame.getLocalPlayer(), game.getId());
-        
+
         timer.cancel();
         if(getSelectColorView().isModalShowing())
         {
-        	getSelectColorView().closeModal();
+            getSelectColorView().closeModal();
         }
         if(getJoinGameView().isModalShowing())
         {
-        	getJoinGameView().closeModal();
+            getJoinGameView().closeModal();
         }
         joinAction.execute();
 
-	}
+    }
 
-	@Override
-	public void update(Observable arg0, Object arg1)
-	{
-		if(arg1 != null)
+    @Override
+    public void update(Observable arg0, Object arg1)
+    {
+        if(arg1 != null)
         {
             if(getJoinGameView().isModalShowing())
             {
@@ -414,8 +386,7 @@ public class JoinGameController extends Controller implements IJoinGameControlle
             {
                 getSelectColorView().closeModal();
             }
-        }		
-	}
+        }
+    }
 
 }
-
