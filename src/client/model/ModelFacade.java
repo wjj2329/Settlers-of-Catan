475e9f01366ebb0b56baaf651765470204afcfd7
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-20 18:44:50 -0600

    idk

--- src/client/model/ModelFacade.java
+++ src/client/model/ModelFacade.java
@@ -503,7 +503,16 @@ public void loadGameDifferentJson(JSONObject mygame) throws JSONException {
 			Index owner = new Index(obj.getInt("owner"));
 			settle1.setOwner(owner);
 			mylocation.setSettlement(settle1);
+			if (currentgame.getMyplayers().get(owner) != null)
+			{
+				System.out.println("we cool");
+			}
+			else
+			{
+				System.out.println("eff");
+			}
 			currentgame.getMyplayers().get(owner).addToSettlements(settle1);
+			//facace_currentgame.currentgame.getMyplayers().get(owner).addToSettlements(settle1);
 			// Alex you need to do something that's not this or maybe inialize it or something
 		}
 		JSONArray cities = map.getJSONArray("cities");
