a977fed2cd9c43676e5698b1c17d1fc1f0b9f232
MasqueradeOfSilence <alexcn711@gmail.com>
2016-05-21 17:01:47 -0600

    minor things.

--- src/client/communication/GameHistoryController.java
+++ src/client/communication/GameHistoryController.java
@@ -30,14 +30,15 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 		//<temp>
 		
 		List<LogEntry> entries = new ArrayList<LogEntry>();
-		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
+		entries.add(new LogEntry(CatanColor.BLUE, "Welcome to Catan! :D "));
+		/*entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
 		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
 		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
 		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
 		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
 		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
 		entries.add(new LogEntry(CatanColor.BROWN, "This is a brown message"));
-		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));
+		entries.add(new LogEntry(CatanColor.ORANGE, "This is an orange message ss x y z w.  This is an orange message.  This is an orange message.  This is an orange message."));*/
 		
 		getView().setEntries(entries);
 	
