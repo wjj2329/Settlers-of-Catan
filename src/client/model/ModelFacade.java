d84a969734223f880d2b2b4e8bee83375eac5bea
wjj2329 <wjj2329@gmail.com>
2016-05-21 17:28:53 -0600

    Correct colors Overlay now yea!!!!

--- src/client/model/ModelFacade.java
+++ src/client/model/ModelFacade.java
@@ -622,9 +622,9 @@ public void loadGameDifferentJson(JSONObject mygame) throws JSONException {
 		
 				newDevCards(obj.getJSONObject("newDevCards"), newPlayer);
 				oldDevCards(obj.getJSONObject("oldDevCards"), newPlayer);
-				JSONObject resources = obj.getJSONObject("Resources");
-				newPlayer.setResources(new ResourceList(resources.getInt("brick"), resources.getInt("ore"),
-						resources.getInt("sheep"), resources.getInt("wheat"), resources.getInt("wood")));
+
+				newPlayer.setResources(new ResourceList());
+				resources(obj, newPlayer.getResources());
 				//resources(obj, newPlayer.getResources());
 				currentgame.addPlayer(newPlayer);
 
