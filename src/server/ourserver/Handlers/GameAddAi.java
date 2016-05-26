f9d119bccdd37a6dd2b9a52822dde247c8eb81fd
wjj2329 <wjj2329@gmail.com>
2016-05-26 12:32:24 -0600

    All the handlers created will now add their Javadoc stuff

---
+++ src/server/ourserver/Handlers/GameAddAi.java
@@ -0,0 +1,8 @@
+package server.ourserver.Handlers;
+
+/**
+ * Created by williamjones on 5/26/16.
+ */
+
+public class GameAddAi {
+}
---
+++ src/server/ourserver/Handlers/MovesMonument.java
@@ -0,0 +1,7 @@
+package server.ourserver.Handlers;
+
+/**
+ * Created by williamjones on 5/26/16.
+ */
+public class MovesMonument {
+}
