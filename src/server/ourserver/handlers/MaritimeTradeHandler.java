7bc68eaaf488546bf7ed59cd3a4423d873916f34
wjj2329 <wjj2329@gmail.com>
2016-05-26 15:41:10 -0600

    Comments

--- src/server/ourserver/handlers/MaritimeTradeHandler.java
+++ src/server/ourserver/handlers/MaritimeTradeHandler.java
@@ -4,6 +4,22 @@ import java.io.IOException;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+/**
+ *  type name of move being executed
+ * playerIndex the player's position in the game's turn order
+ *  ratio integer(2,3, or 4)
+ *  inputResource what you are giving
+ *  outputResource what you are getting
+ *
+ *
+ * @pre it is your turn, the client model's status is 'Playing'
+ * 						you have the resources you are giving, for ratios less than 4, you
+ * 						have the correct port for the trade
+ * @post the trade has been executed ( offered resources are in the bank, and the requested resource
+ * 						has been received)
+ *
+ *
+ */
 
 public class MaritimeTradeHandler implements HttpHandler
 {
--- src/server/ourserver/handlers/YearOfPlentyHandler.java
+++ src/server/ourserver/handlers/YearOfPlentyHandler.java
@@ -5,6 +5,19 @@ import java.io.IOException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+/**
+ *  type name of move being executed
+ * playerIndex the player's position in the game's turn order
+ *  resource1 first resource you want to receive
+ * resource2 second resource you want to receive
+ *
+ * @pre  it is your turn, the client model's status is 'Playing'
+ * 						you have the card you want to play in old dev card hand,
+ * 						two specified resources are in the bank
+ * @post you gained to specified resources
+ *
+ *
+ */
 public class YearOfPlentyHandler implements HttpHandler
 {
 	@Override
