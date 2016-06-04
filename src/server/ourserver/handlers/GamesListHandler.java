d699ea77cf17326036a1f985263b820b8bb395a6
Matt <matheusavpacheco@gmail.com>
2016-06-03 20:29:53 -0600

    mhm

--- src/server/ourserver/handlers/GamesListHandler.java
+++ src/server/ourserver/handlers/GamesListHandler.java
@@ -20,7 +20,7 @@ import org.json.JSONArray;
  * @post If the operation succeeds, 1.The server returns an
  *                HTTP 200 success response. 2.The body contains a JSON
  *                array containing a list of objects that contain
- *                information about the serverâ€™s games @post If the operation
+ *                information about the server’s games @post If the operation
  *                fails, 1.The server returns an HTTP 400 error response,
  *                and the body contains an error message
  *
@@ -33,14 +33,14 @@ public class GamesListHandler implements HttpHandler
        	JSONArray games = ServerFacade.getInstance().getGameList();
         
         if(games == null){
-            httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
             String response = "Could not get game list";
             httpExchange.getResponseBody().write(response.getBytes());
+            httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
             return;
         }
+        httpExchange.getResponseHeaders().add("Content-type", "application/json");
         httpExchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
         httpExchange.getResponseBody().write(games.toString().getBytes());
-       
         httpExchange.close();
     }
 }
