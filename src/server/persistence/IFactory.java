f2c1f440a57d41cd43ffd768327472fff825adf9
guzmanz <zulguzman@yahoo.com>
2016-06-07 21:22:13 -0600

    some stuff that I think has to be in here

--- src/server/persistence/IFactory.java
+++ src/server/persistence/IFactory.java
@@ -1,7 +1,23 @@
 package server.persistence;
 
+import shared.chat.GameHistoryLine;
+
 /**
  * Created by williamjones on 6/7/16.
  */
 public interface IFactory {
+	
+	//I think these have to be in here? 
+	/**
+	 * This gets the Game Manager thing
+	 * @return
+	 */
+	public IGameManager getGameManager();
+	
+	/**
+	 * This gets the User Account stuff
+	 * @return
+	 */
+	public IUserAccount getUserAccount();
+	
 }
--- src/server/persistence/TextDBFactory.java
+++ src/server/persistence/TextDBFactory.java
@@ -7,5 +7,16 @@ public class TextDBFactory implements IFactory
 {
     TextDBGameManagerDAO mymanager=new TextDBGameManagerDAO();
     TextDBUserAccountsDAO textDBUserAccountsDAO=new TextDBUserAccountsDAO();
+    
+	@Override
+	public IGameManager getGameManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public IUserAccount getUserAccount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
