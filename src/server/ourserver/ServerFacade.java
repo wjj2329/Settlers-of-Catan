727b9da08462f3ed860bd6ce0a2a43aa15f6b3d3
wjj2329 <wjj2329@gmail.com>
2016-06-11 19:38:13 -0600

    Catan Game now has update from JSON method

--- src/server/ourserver/ServerFacade.java
+++ src/server/ourserver/ServerFacade.java
@@ -121,6 +121,8 @@ public class ServerFacade
 		allRegisteredUsers.add(mark);
 		allRegisteredUsers.add(brooke);
 		allRegisteredUsers.add(pete);
+
+
 		
 		try {
 			loadallplayersfromtextdatabase();
